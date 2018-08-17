40c9a49ed02eaed4b6a7cea7c71fb795fa2b7f81
zhangcz <1299670108@qq.com>
2018-08-17 14:33:35 +0800

    增加显示天气的功能

---
+++ app/src/main/java/com/leitingpro/coolweather/Constent.java
@@ -0,0 +1,7 @@
+package com.leitingpro.coolweather;
+
+public class Constent {
+    public static final String KEY = "a3d90f0943c84b4d9bc9385eaf62cbba";
+    public static final String WEATHER_UTL= "http://guolin.tech/api/weather?cityid=";
+    public static final String BING_PIC = "http://guolin.tech/api/bing_pic";
+}
--- app/src/main/java/com/leitingpro/coolweather/util/Utility.java
+++ app/src/main/java/com/leitingpro/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.leitingpro.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.leitingpro.coolweather.db.City;
 import com.leitingpro.coolweather.db.County;
 import com.leitingpro.coolweather.db.Province;
+import com.leitingpro.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -87,4 +89,22 @@ public class Utility {
         }
         return false;
     }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     * @param response
+     * @return
+     */
+    public static Weather handWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return  new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
